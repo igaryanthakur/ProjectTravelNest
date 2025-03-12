18019e337687f26cf7ff76e2c880a52ee00b9f55
Aryan <aryankthakur2004@gmail.com>
2025-03-12 22:23:52 +0530

    Mapbox added

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listings");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAPBOX_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
@@ -25,12 +28,19 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
+
   let url = req.file.path;
   let filename = req.file.filename;
-
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
+  newListing.geometry = response.body.features[0].geometry;
   await newListing.save();
   req.flash("success", "Successfully created a new listing!");
   res.redirect("/listings");
@@ -46,10 +56,9 @@ module.exports.renderEditForm = async (req, res) => {
   let originalImageUrl = listing.image.url;
   originalImageUrl = originalImageUrl.replace(
     "upload",
-    "upload/ar_1.0,c_fill,h_250/bo_5px_solid_lightblue"
+    "upload/ar_1.0,c_fill,h_200,w_200/bo_5px_solid_lightblue"
   );
-
-  res.render("listings/edit.ejs", { listing, originalImageUrl});
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
--- init/index.js
+++ init/index.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listings.js");
+require('dotenv').config();
+console.log("Mapbox Token from .env:", process.env.MAPBOX_TOKEN);
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAPBOX_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 
 const Mongo_Url = "mongodb://localhost:27017/travelnest";
 
@@ -18,10 +24,21 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({
-    ...obj,
-    owner: "67cef00492794922b75b05f2",
-  }));
+  for (let obj of initData.data) {
+    let response = await geocodingClient
+      .forwardGeocode({
+        query: obj.location,
+        limit: 1,
+      })
+      .send();
+    const geometry = response.body.features[0].geometry || {
+      type: "Point",
+      coordinates: [0, 0],
+    };
+
+    obj.owner = "67cef00492794922b75b05f2";
+    obj.geometry = geometry;
+  }
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
